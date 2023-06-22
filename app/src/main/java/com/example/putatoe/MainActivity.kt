7151a9b963847ba0231712e499aa493855c0cd53
Siddharth Bansal <chinubansal766@gmail.com>
2023-06-23 01:45:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/putatoe/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.putatoe
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
